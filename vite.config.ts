256d36750a7fe12feb6367b4e7ae7222153134b9
Tú GiNô <nguyendinhtu.job@gmail.com>
2024-11-16 01:23:23 +0700

    fix: 🐛 path build

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Cho phép thằng Vite sử dụng được process.env, mặc định thì không mà sẽ phải dùng import.meta.env
-  // https://github.com/vitejs/vite/issues/1973
-  define: {
-    'process.env': process.env
-  },
+  base: '/',
   plugins: [
     react(),
     svgr()
